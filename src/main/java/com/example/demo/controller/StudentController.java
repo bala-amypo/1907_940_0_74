7a7ef17ae01630fbc58232b6d1d43fedf49bbca0
bala-amypo <bala@amypo.in>
2025-12-18 10:27:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,19 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.swathi.entity.Student;
-import com.example.swathi.service.StudentService;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/Studentsdata")
@@ -23,13 +21,13 @@ public class StudentController {
 
     @PostMapping("/postdata")
     public ResponseEntity<Student> postdata(@Valid @RequestBody Student student){
-        return new ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
+        return new  ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
     }
+    
     @GetMapping("/getdata/{id}")
     public Student getdata(@PathVariable Long id){
-        return StudentService.getStudentById(id);
+         return studentService.getStudentById(id);
     }
-
-
+    
 
 }
