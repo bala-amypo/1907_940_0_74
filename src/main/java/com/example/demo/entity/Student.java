8ca520ec2ad63cbe1cc1f6ace0cb086f3efa8a26
bala-amypo <bala@amypo.in>
2025-12-18 06:36:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,18 +1,32 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+//import jakarta.persistence.Table;
 @Entity
 //@Table(name="StudentTable")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
+    @NotBlank(message = "Name filed cannot be empty")
+    @Size(min=3,max=20,message = "The user name must be min of 3 and max of 20 character")
     private String name;
+    @Email(message = "Invalid email id")
+    @Column(unique = true)
     private String email;
+    //@NotNull
+    //@Size(min,max)
+    //@min
+    //@max
+    //@pattern(reg)
+
     
     
     public void setId(Long id){
