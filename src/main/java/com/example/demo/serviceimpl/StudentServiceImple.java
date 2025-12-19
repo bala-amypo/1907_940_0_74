020f4f0b52909489a5c2ed5864420dc24cd9bda5
bala-amypo <bala@amypo.in>
2025-12-19 06:03:49 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -4,29 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRespository;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 import com.example.demo.exception.ResourceNotFoundException;
 
 @Service
 
 public class StudentServiceImple implements StudentService {
-    // private final StudentRespository studentRespository;
-    // public StudentServiceImple(StudentRespository studentRespository){
-     //   this.studentRespository=studentRespository;
+    // private final StudentRepository studentRepository;
+    // public StudentServiceImple(StudentRepository studentRepository){
+     //   this.studentRespoitory=studentRepository;
     // }
     @Autowired
-    StudentRespository studentRespository;
+    StudentRepository studentRepository;
  
     public Student saveStudent(Student student) {
 
-        return studentRespository.save(student);
+        return studentRepository.save(student);
     }
     public Student getStudentById(Long id){
-        return studentRespository.findById(id)
+        return studentRepository.findById(id)
         .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
     }
-
-
+    public Student updatedata(Long id, Student student) {
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRepository.save(exits);
+        // orElseThrow(()-> new ResourceNotFoundException( "Student not found"));
+    } 
+    public void deletedata(Long id) {
+        Student student=getStudentById(id);
+            studentRepository.delete(student);
+    } 
 }
-
