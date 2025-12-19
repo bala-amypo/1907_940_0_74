aa7397c66c2052444cbe2a8d2f77818ad4dd48fa
bala-amypo <bala@amypo.in>
2025-12-19 06:06:49 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -4,37 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepository;
+import com.example.demo.repository.StudentRespository;
 import com.example.demo.service.StudentService;
 import com.example.demo.exception.ResourceNotFoundException;
 
 @Service
 
 public class StudentServiceImple implements StudentService {
-    // private final StudentRepository studentRepository;
-    // public StudentServiceImple(StudentRepository studentRepository){
+    // private final StudentRespository studentRepository;
+    // public StudentServiceImple(StudentRespository studentRepository){
      //   this.studentRespoitory=studentRepository;
     // }
     @Autowired
-    StudentRepository studentRepository;
+    StudentRespository studentRespository;
  
     public Student saveStudent(Student student) {
 
-        return studentRepository.save(student);
+        return studentRespository.save(student);
     }
     public Student getStudentById(Long id){
-        return studentRepository.findById(id)
+        return studentRespository.findById(id)
         .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
     }
     public Student updatedata(Long id, Student student) {
         Student exits=getStudentById(id);
         exits.setName(student.getName());
         exits.setEmail(student.getEmail());
-        return studentRepository.save(exits);
+        return studentRespository.save(exits);
         // orElseThrow(()-> new ResourceNotFoundException( "Student not found"));
     } 
     public void deletedata(Long id) {
         Student student=getStudentById(id);
-            studentRepository.delete(student);
+            studentRespository.delete(student);
     } 
 }
